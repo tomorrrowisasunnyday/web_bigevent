6b0dfa75336fbc190d113f8e77dffe9f519b941c
tomorrrowisasunnyday <1553438969@qq.com>
2022-03-18 11:36:34 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    // 调用getUserInfo函数调用用户基本信息
+    getUserInfo()
+
+    // 退出功能
+    var layer = layui.layer
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            // 清除本地存储的token
+            localStorage.removeItem('token')
+            // 重新跳转到登录页面
+            location.href = '/login.html'
+            layer.close(index);
+          });
+    })
+})
+
+//获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用renderAvatar渲染用户头像
+            renderAvatar(res.data)
+        },
+        
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    var name = user.username || user.nickname;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
