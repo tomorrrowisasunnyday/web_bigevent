6b34a0a75e076b649b7551ed92b984ebe9207e99
tomorrrowisasunnyday <1553438969@qq.com>
2022-03-20 20:41:10 +0800

    完成了文章管理相关功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,6 +4,7 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // options.url = 'http://ajax.frontend.itheima.net' + options.url;
     // 统一为有权限的接口，设置header请求头
     if (options.url.indexOf('/my/' !== -1)) {
         options.headers = {
